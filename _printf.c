1b9b1e874f67393e7954f7368d148b7db7ecf1f5
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-03-24 16:06:50 -0700

    setup project setup header files create _putchar create _printf'

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - prints formated strings to output stream
+ *
+ * @format: string of characters
+ *
+ * Return: int - number of characters printed (excluding the 
+ * null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+
+}
