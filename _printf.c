7f91e85c8ec9bd45d305035a75ce1cacee1e96e0
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-03-26 02:16:08 +0200

    bug fixes

--- _printf.c
+++ _printf.c
@@ -18,6 +18,9 @@ int _printf(const char *format, ...)
 	if (!format)
 		return (-1);
 
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '\0')
