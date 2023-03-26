0c647f90bd9f47bead674dba7302998a313624dc
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-03-26 04:00:55 +0200

    proposed fix for %s and %c

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (!format)
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 
 	for (i = 0; format[i] != '\0'; i++)
