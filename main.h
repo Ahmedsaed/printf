f53e7ded2acb296eeabcd6635195fff0c9914c29
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-03-28 15:17:49 +0200

    Fix to satisfy checker, #0

--- main.h
+++ main.h
@@ -12,7 +12,9 @@ int print_str(char *s, char *buffer, int *index);
 int print_integer(long int n, char *buffer, int *index);
 unsigned int print_binary(unsigned int k, char *buffer, int *index);
 unsigned int print_octal(unsigned int k, char *buffer, int *index);
-unsigned int print_hex(unsigned int k, int char_case, char *buffer, int *index);
+unsigned int print_hex(
+		unsigned int k, int char_case,
+		char *buffer, int *index);
 int print_address(void *p, char *buffer, int *index);
 int print_non_printable(char *str, char *buffer, int *index);
 int flush_buffer(char buffer[], int *index);
